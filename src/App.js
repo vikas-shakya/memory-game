ddbf5984e570fbc363684fae6771a9c79124a585
vikas-shakya <vikasshakya9870@gmail.com>
2024-12-06 14:29:44 +0530

    Update App.js
    done, still left to complete for hosting

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ const cardImages = [
 
 function App() {
   const [cards, setCards] = useState([]);
-  // const [turns, setTurns] = useState(0);
-  // const [choiceOne, setChoiceOne] = useState(null);
-  // const [choiceTwo, setChoiceTwo] = useState(null);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   const handleChoice = (card) => {
     console.log(card);
