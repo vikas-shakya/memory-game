62f9757c882007789e27ae17e264eb67bc73760c
vikas-shakya <vikasshakya9870@gmail.com>
2024-12-06 14:19:42 +0530

    Update App.js
    made some changes to deploy to netlify

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
-  const [choiceOne, setChoiceOne] = useState(null);
-  const [choiceTwo, setChoiceTwo] = useState(null);
+  // const [choiceOne, setChoiceOne] = useState(null);
+  // const [choiceTwo, setChoiceTwo] = useState(null);
 
   const handleChoice = (card) => {
     console.log(card);
